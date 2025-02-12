e3b22bbc2c4003d7f1b76e6c82d928c96bde94a6
Keven.ReyesGlobant <keven.reyes@globant.com>
2025-02-12 16:35:51 -0500

    Create model client

--- clients/clients/src/main/java/model/entity/Client.java
+++ clients/clients/src/main/java/model/entity/Client.java
@@ -1,4 +1,34 @@
 package model.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "client")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotEmpty(message = "This field is not empty")
+    private String name;
+    @Column(unique = true)
+    @Email
+    @NotEmpty
+    private String email;
+    @NotBlank
+    private String document;
+    @NotBlank
+    private String phone;
+    @NotBlank
+    private String deliveryAddress;
+
 }
