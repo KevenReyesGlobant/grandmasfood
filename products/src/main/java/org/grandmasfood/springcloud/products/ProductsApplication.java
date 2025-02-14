a8414822cf4392851152f1df5d4cc044030f059f
Keven.ReyesGlobant <keven.reyes@globant.com>
2025-02-14 12:22:29 -0500

    Request GET products

--- products/src/main/java/org/grandmasfood/springcloud/products/ProductsApplication.java
+++ products/src/main/java/org/grandmasfood/springcloud/products/ProductsApplication.java
@@ -1,4 +1,4 @@
-package org.grandmasfood.springcloud.products.products;
+package org.grandmasfood.springcloud.products;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- products/src/main/java/org/grandmasfood/springcloud/products/controller/ProductsController.java
+++ products/src/main/java/org/grandmasfood/springcloud/products/controller/ProductsController.java
@@ -1,7 +1,14 @@
 package org.grandmasfood.springcloud.products.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProductsController {
+
+    @GetMapping
+    public ResponseEntity<?> message(){
+        return ResponseEntity.ok("Hello from Products");
+    }
 }
