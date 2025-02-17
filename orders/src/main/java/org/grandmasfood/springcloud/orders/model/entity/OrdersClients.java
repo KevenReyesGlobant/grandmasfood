65b923c25a4c479351cfdcbedea4b7b53dd1e28b
Keven.ReyesGlobant <keven.reyes@globant.com>
2025-02-17 09:57:26 -0500

    Create relacion into entities

--- orders/src/main/java/org/grandmasfood/springcloud/orders/model/entity/OrdersClients.java
+++ orders/src/main/java/org/grandmasfood/springcloud/orders/model/entity/OrdersClients.java
@@ -12,4 +12,16 @@ public class OrdersClients {
     private Long id;
     @Column(name = "order_id", unique = true)
     private Long clientId;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+
+        if (!(o instanceof OrdersClients)) return false;
+
+        OrdersClients obj = (OrdersClients) o;
+
+        return this.clientId != null && this.clientId.equals(obj.clientId);
+
+    }
 }
