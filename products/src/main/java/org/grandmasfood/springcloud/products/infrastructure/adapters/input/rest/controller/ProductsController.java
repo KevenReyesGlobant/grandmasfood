a69340004297b9365d47de57c6e4bee0bc082300
Keven.ReyesGlobant <keven.reyes@globant.com>
2025-03-11 11:29:47 -0500

    Migrate data product microservices

--- products/src/main/java/org/grandmasfood/springcloud/products/infrastructure/adapters/input/rest/controller/ProductsController.java
+++ products/src/main/java/org/grandmasfood/springcloud/products/infrastructure/adapters/input/rest/controller/ProductsController.java
@@ -1,9 +1,29 @@
 package org.grandmasfood.springcloud.products.infrastructure.adapters.input.rest.controller;
 
 
+import jakarta.validation.Valid;
+import org.grandmasfood.springcloud.products.application.ports.input.ProductServicesPort;
+import org.grandmasfood.springcloud.products.infrastructure.adapters.input.rest.mapper.ProductRestMapper;
+import org.grandmasfood.springcloud.products.infrastructure.adapters.input.rest.model.request.ProductsCreateRequestDTO;
+import org.grandmasfood.springcloud.products.infrastructure.adapters.input.rest.model.response.ProductResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
 @RestController
 public class ProductsController {
+    private final ProductServicesPort productServicesPort;
+    private final ProductRestMapper productRestMapper;
+
+    public ProductsController(ProductServicesPort productServicesPort, ProductRestMapper productRestMapper) {
+        this.productServicesPort = productServicesPort;
+        this.productRestMapper = productRestMapper;
+    }
+
+    @PostMapping("/product")
+    public ResponseEntity<ProductResponse> createProduct(@RequestBody @Valid ProductsCreateRequestDTO productsCreateRequestDTO) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(productRestMapper.toProductResponseDTO(productServicesPort.save(productRestMapper.toProduct(productsCreateRequestDTO))));
+
+    }
 }
--- products/src/main/java/org/grandmasfood/springcloud/products/infrastructure/adapters/input/rest/mapper/ProductRestMapper.java
+++ products/src/main/java/org/grandmasfood/springcloud/products/infrastructure/adapters/input/rest/mapper/ProductRestMapper.java
@@ -1,6 +1,5 @@
 package org.grandmasfood.springcloud.products.infrastructure.adapters.input.rest.mapper;
 
-import ch.qos.logback.core.net.server.Client;
 import org.grandmasfood.springcloud.products.domain.model.Product;
 import org.grandmasfood.springcloud.products.infrastructure.adapters.input.rest.model.request.ProductsCreateRequestDTO;
 import org.grandmasfood.springcloud.products.infrastructure.adapters.input.rest.model.response.ProductResponse;
@@ -8,7 +7,7 @@ import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface ProductRestMapper {
-    
+
     Product toProduct(ProductsCreateRequestDTO productsCreateRequestDTO);
 
     ProductResponse toProductResponseDTO(Product product);
