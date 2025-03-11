714e673c6cb0b0f5d1ce25c5c592cb6459c8ac6b
Keven.ReyesGlobant <keven.reyes@globant.com>
2025-03-11 10:44:39 -0500

    Migrate data product microservices

--- products/src/main/java/org/grandmasfood/springcloud/products/application/service/ProductsService.java
+++ products/src/main/java/org/grandmasfood/springcloud/products/application/service/ProductsService.java
@@ -1,14 +1,39 @@
 package org.grandmasfood.springcloud.products.application.service;
 
 import org.grandmasfood.springcloud.products.application.ports.input.ProductServicesPort;
+import org.grandmasfood.springcloud.products.application.ports.output.ProductPersistentPort;
+import org.grandmasfood.springcloud.products.domain.exceptions.ProductNotFoundException;
 import org.grandmasfood.springcloud.products.domain.model.Product;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.UUID;
+import java.util.function.Consumer;
 
 @Service
 public class ProductsService implements ProductServicesPort {
+
+    private final ProductPersistentPort productPersistentPort;
+
+    public ProductsService(ProductPersistentPort productPersistentPort) {
+        this.productPersistentPort = productPersistentPort;
+    }
+
+    @Override
+    public Product save(Product product) {
+        return productPersistentPort.save(product);
+    }
+
+    @Override
+    public Product findActiveByUuid(UUID uuid) {
+        return productPersistentPort.findActiveByUuid(uuid).orElseThrow(ProductNotFoundException::new);
+    }
+
+    @Override
+    public Product findActiveById(Long id) {
+        return productPersistentPort.findActiveById(id).orElseThrow(ProductNotFoundException::new);
+    }
+
 
     @Override
     public Product findById(Long id) {
@@ -20,28 +45,28 @@ public class ProductsService implements ProductServicesPort {
         return List.of();
     }
 
-    @Override
-    public Product save(Product product) {
-        return null;
-    }
 
     @Override
     public Product update(UUID uuid, Product product) {
-        return null;
+        return productPersistentPort.findActiveByUuid(uuid).map(saveProduct -> {
+            updateField(product.getFantasyName(), saveProduct::setFantasyName);
+            updateField(product.getCategory(), saveProduct::setCategory);
+            updateField(product.getDescription(), saveProduct::setDescription);
+            updateField(product.getPrice(), saveProduct::setPrice);
+            return productPersistentPort.save(saveProduct);
+        }).orElseThrow(ProductNotFoundException::new);
     }
 
-    @Override
-    public Product findActiveByUuid(UUID Uuid) {
-        return null;
+
+    private <T> void updateField(T newValue, java.util.function.Consumer<T> setter) {
+        if (newValue != null) {
+            setter.accept(newValue);
+        }
     }
 
-    @Override
-    public Product findActiveById(Long id) {
-        return null;
-    }
 
     @Override
     public Product deleteByUuid(UUID uuid) {
-        return null;
+        return productPersistentPort.deleteByUuid(uuid);
     }
 }
