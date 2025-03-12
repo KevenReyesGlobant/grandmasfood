36296fbd33a72bf62b63b2a70cd35f7448d724e0
Keven.ReyesGlobant <keven.reyes@globant.com>
2025-03-12 16:10:29 -0500

    Migrate data order  microservices

--- orders/src/main/java/org/grandmasfood/springcloud/orders/domain/validations/DeliveryDateValidator.java
+++ orders/src/main/java/org/grandmasfood/springcloud/orders/domain/validations/DeliveryDateValidator.java
@@ -1,37 +1,39 @@
 package org.grandmasfood.springcloud.orders.domain.validations;
 
-import jakarta.validation.ConstraintDefinitionException;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 import org.grandmasfood.springcloud.orders.application.ports.input.IValidDeliveryDate;
-import org.grandmasfood.springcloud.orders.domain.model.Order;
+import org.grandmasfood.springcloud.orders.infrastructure.adapters.input.rest.model.request.OrdersCreateRequestDTO;
 
-public class DeliveryDateValidator implements ConstraintValidator<IValidDeliveryDate, Order> {
-
+public class DeliveryDateValidator implements ConstraintValidator<IValidDeliveryDate, OrdersCreateRequestDTO> {
 
     @Override
     public void initialize(IValidDeliveryDate constraintAnnotation) {
-        ConstraintValidator.super.initialize(constraintAnnotation);
+        // No initialization needed
     }
 
     @Override
-    public boolean isValid(Order order, ConstraintValidatorContext context) {
-        // Si delivered es false, deliveryDate debe ser null
-        if (Boolean.FALSE.equals(order.isDelivered())) {
-            if (order.getDeliveryDate() != null) {
-                throw new ConstraintDefinitionException("If delivered is false, delivery date must be null");
-            }
-            return true;
+    public boolean isValid(OrdersCreateRequestDTO order, ConstraintValidatorContext context) {
+        if (order == null) {
+            return true; // Consider null value as valid
         }
 
-        // Si delivered es true, deliveryDate no puede ser null
-        if (Boolean.TRUE.equals(order.isDelivered()) && order.getDeliveryDate() == null) {
-            throw new ConstraintDefinitionException("If delivered is true, delivery date cannot be empty");
+        if (!order.getDelivered() && order.getDeliveryDate() != null) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate("If delivered is false, delivery date must be null")
+                    .addPropertyNode("deliveryDate")
+                    .addConstraintViolation();
+            return false;
+        }
+
+        if (order.getDelivered() && order.getDeliveryDate() == null) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate("If delivered is true, delivery date cannot be empty")
+                    .addPropertyNode("deliveryDate")
+                    .addConstraintViolation();
+            return false;
         }
 
         return true;
     }
-
-
-
 }
