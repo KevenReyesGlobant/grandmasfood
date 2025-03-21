191ab22a443734e5e315244ae28e782c33610601
Keven.ReyesGlobant <keven.reyes@globant.com>
2025-03-21 10:05:20 -0500

    Filter values isAvailable

--- products/src/main/java/org/grandmasfood/springcloud/products/infrastructure/adapters/input/rest/generated/GeneratedPdfBox.java
+++ products/src/main/java/org/grandmasfood/springcloud/products/infrastructure/adapters/input/rest/generated/GeneratedPdfBox.java
@@ -16,7 +16,6 @@ import org.springframework.stereotype.Component;
 
 import java.awt.*;
 import java.io.IOException;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
