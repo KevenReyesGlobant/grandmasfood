dff348fd50ca832ebd2a43dff432ea0fad177c36
Keven.ReyesGlobant <keven.reyes@globant.com>
2025-03-23 15:26:01 -0500

    Config swagger all microservices

---
+++ orders/src/main/java/org/grandmasfood/springcloud/orders/infrastructure/adapters/swagger/SwaggerConfig.java
@@ -0,0 +1,38 @@
+package org.grandmasfood.springcloud.orders.infrastructure.adapters.swagger;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "API GRANDMASFOOD_ORDERS",
+                description = "This app microservices provides a clients management system",
+                version = "1.0.0",
+                contact = @Contact(
+                        name = "Keven Reyes",
+                        url = "https://portfoliothreekevenreyes.netlify.app/",
+                        email = "keven.reyes@globant.com"
+                ),
+                license = @License(
+                        name = "Standard Software Use License for Grandmasfood",
+                        url = "https://portfoliothreekevenreyes.netlify.app/"
+                )
+        ),
+        servers = {
+                @Server(
+                        description = "DEV SERVER",
+                        url = "http://localhost:3000"
+                ),
+                @Server(
+                        description = "PROD SERVER",
+                        url = "http://localhost:3000"
+                )
+        }
+)
+
+public class SwaggerConfig {
+
+}
