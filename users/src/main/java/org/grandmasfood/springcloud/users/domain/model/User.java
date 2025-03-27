a91a2497862e5b241653684b00272f2f76164aa8
Keven.ReyesGlobant <keven.reyes@globant.com>
2025-03-27 11:58:45 -0500

    Create model users

--- users/src/main/java/org/grandmasfood/springcloud/users/domain/model/User.java
+++ users/src/main/java/org/grandmasfood/springcloud/users/domain/model/User.java
@@ -1,4 +1,30 @@
 package org.grandmasfood.springcloud.users.domain.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.grandmasfood.springcloud.users.domain.model.enums.RoleUser;
+
+import java.util.UUID;
+
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class User {
+
+    private Long idUser;
+    private UUID uuid;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private Boolean verified;
+    private RoleUser roleUser;
+    private Boolean active;
+
 }
+
