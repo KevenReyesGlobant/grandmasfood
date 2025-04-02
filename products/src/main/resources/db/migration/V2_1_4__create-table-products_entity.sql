348b9977c6a338113eee25c9942d74d38f1e57da
Keven.ReyesGlobant <keven.reyes@globant.com>
2025-04-02 11:58:40 -0500

    Insert migration clients and orders

---
+++ products/src/main/resources/db/migration/V2_1_4__create-table-products_entity.sql
@@ -0,0 +1,15 @@
+CREATE TABLE products_entity (
+    id_product BIGINT PRIMARY KEY AUTO_INCREMENT,
+    uuid VARCHAR(36) NOT NULL,
+    fantasy_name VARCHAR(255) NOT NULL,
+    category VARCHAR(50) NOT NULL CHECK (
+        category IN (
+            'HAMBURGERS_AND_HOTDOGS', 'CHICKEN', 'FISH', 'MEATS',
+            'DESSERTS', 'VEGAN_FOOD', 'KIDS_MEALS'
+        )
+    ) DEFAULT 'HAMBURGERS_AND_HOTDOGS',
+    description VARCHAR(511) NOT NULL,
+    price FLOAT NOT NULL CHECK (price > 0),
+    available BOOLEAN DEFAULT FALSE,
+    active BOOLEAN DEFAULT TRUE
+);
