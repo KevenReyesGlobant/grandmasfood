9e1a4410b01cd401741de71feadd04cee575f0ec
Keven.ReyesGlobant <keven.reyes@globant.com>
2025-05-26 16:46:43 -0500

    Initial config kafka

--- manage/src/main/java/org/grandmasfood/springcloud/manage/ManageApplication.java
+++ manage/src/main/java/org/grandmasfood/springcloud/manage/ManageApplication.java
@@ -2,9 +2,15 @@ package org.grandmasfood.springcloud.manage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.KafkaListener;
 
 @SpringBootApplication
 public class ManageApplication {
+
+    @KafkaListener(topics = "devs4j-topic", groupId = "devs4j-group")
+    public void listen(String message) {
+        System.out.println("Received Messasge in group foo: " + message);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ManageApplication.class, args);
